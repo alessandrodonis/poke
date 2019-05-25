d00fdb7f03c60f57c0c78deeec4da4618f6b4c11
alessandrodonis <alessandrodonis17@gmail.com>
2019-05-25 16:50:08 -0600

    Siu

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var pokemon= angular.module("pokemon", [])
  
-pokemon.controller("pokedex", fuction($scope,$rootScope,$http){
+pokemon.controller("pokedex", function($scope,$rootScope,$http){
 	$http({
 		method: "GET",
-		url: "https://pokeapi.co/api/v2//pokemon/25"
+		url: "https:/pokeapi.co/api/v2/pokemon/25"
 	}).then(function callbackSucces (y){
 		console.log(y);
 	})
